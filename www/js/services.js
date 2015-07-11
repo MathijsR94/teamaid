f95b62aaa667518ed54483ba07976c33222eeb87
MathijsR94 <mathijsrutgers@gmail.com>
2015-07-11 10:18:15 +0200

    fireBaseData toegevoegd

--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,45 @@
 angular.module('starter.services', [])
-.factory('firebaseRef', function(){
-    var ref = new Firebase("https://amber-torch-2058.firebaseio.com/");
-    return {
-        ref: function(){
-            return ref;
+    .factory('firebaseRef', function () {
+        var ref = new Firebase("https://amber-torch-2058.firebaseio.com/");
+        return {
+            ref: function () {
+                return ref;
+            }
         }
-    }
-})
+    })
+
+    .factory('fireBaseData', function (firebaseRef) {
+        var ref = firebaseRef.ref();
+        return {
+            ref: function () {
+                return ref;
+            },
+            user: function () {
+                return ref.getAuth();
+            },
+            logout: function () {
+                ref.unauth()
+            },
+            resetPassword: function (email) {
+                ref.resetPassword({
+                    email: email
+                }, function (error) {
+                    if (error) {
+                        switch (error.code) {
+                            case "INVALID_USER":
+                                alert("The specified user account does not exist.");
+                                break;
+                            default:
+                                alert("Error resetting password:", error);
+                        }
+                    }
+                    else {
+                        alert("Password reset email sent successfully!");
+                    }
+                });
+            },
+            connectedRef: function () {
+                return ref.child(".info").child("connected");
+            }
+        }
+    })
