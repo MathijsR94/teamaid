e53287b5c9b89d593e8f34186e1cdd3ed4cb33bc
n00dels <koen.zoon@gmail.com>
2015-07-14 21:47:55 +0200

    new

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -55,7 +55,7 @@ angular.module('starter.controllers', [])
                                 var usersRef = fireBaseData.ref().child("Users");
                                 var uid = authData.uid;
                                 var ins = "";
-								var teamId = $scope.teamId;
+								var $teamId = $scope.teamId;
                                 if (insertion != null) ins = insertion;
                                 usersRef.child(uid).set({
                                     firstName: firstName,
@@ -64,13 +64,14 @@ angular.module('starter.controllers', [])
                                     email: em,
                                     registerDate: Firebase.ServerValue.TIMESTAMP
                                 });
+								console.log("dit is het TEamid:" + $teamId);
 								// add team to teams
 								usersRef.child(uid).child("Teams").set({
-									teamId : true
+									$teamId : true
 								});
 								//add admin position 
 								usersRef.child(uid).child("Admins").set({
-									teamId : true
+									$teamId : true
 								});
                                 //Teams.linkPlayer($scope.teamId, uid);
                                 $state.go('app.home');
