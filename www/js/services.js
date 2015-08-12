69f93a74674a82f0bd467b3c69c159d73f8a5c2b
n00dels <z0032ewf@MD1E3DXC.ww002.siemens.net>
2015-08-12 20:32:05 +0200

    update for first webrelease

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('starter.services', [])
     .factory('firebaseRef', function () {
-        var ref = new Firebase("https://amber-torch-2058.firebaseio.com/");
+        var ref = new Firebase("https://teamaid.firebaseio.com/");
         return {
             ref: function () {
                 return ref;
