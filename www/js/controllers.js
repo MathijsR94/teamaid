dd03ef222f49f6ee93b724175d7f71aa9ea48013
n00dels <z0032ewf@MD1E3DXC.ww002.siemens.net>
2015-11-17 21:26:40 +0100

    update linkExternal Players

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1825,11 +1825,14 @@ angular.module('starter.controllers', [])
     })
 
 
-    .controller('SettingsCtrl', function ($scope, fireBaseData, User, Settings, localStorageFactory, firebaseRef) {
+    .controller('SettingsCtrl', function ($scope, fireBaseData, User, Settings, Attendance,Statistics, localStorageFactory, firebaseRef) {
 
         $scope.teamId = localStorageFactory.getTeamId();
         $scope.settings = localStorageFactory.getSettings();
         $scope.isAdmin = localStorageFactory.getAdmin();
+		$scope.players = localStorageFactory.getPlayers();
+		$scope.externalList = {};
+		$scope.nbsp = " ";
 
         $scope.connected = firebaseRef.connectedRef().on("value", function (snap) {
             if (snap.val() === true) {
@@ -1837,6 +1840,19 @@ angular.module('starter.controllers', [])
                     $scope.settings = settingsSnap.val();
                     localStorageFactory.setSettings(settingsSnap.val());
                 });
+				firebaseRef.ref().child("Statistics").child($scope.teamId).once('value', function (statsSnap){
+					$scope.statistics = statsSnap.val();
+					for (gameId in $scope.statistics) {
+						if (typeof $scope.statistics[gameId].externalPlayers !== 'undefined') {
+							// we have external players in this match
+							for( externalId in $scope.statistics[gameId].externalPlayers){
+								$scope.externalList[gameId+"?key?"+externalId] = $scope.statistics[gameId].externalPlayers[externalId].firstName;
+							}
+						}
+					}
+					console.log($scope.externalList);
+				});
+				
             }
         });
         $scope.toggleGroup = function (group) {
@@ -1861,6 +1877,65 @@ angular.module('starter.controllers', [])
                 alert("wachtwoorden zijn niet gelijk");
             }
         };
+		$scope.changeExtInt = function(playerExt,playerInt){
+			if(typeof playerExt === 'undefined' || typeof playerInt === 'undefined'){
+				alert("vul beide velden in");
+			}
+			else{
+				var keys = playerExt.split("?key?");
+				var gameId = keys[0];
+				var extId = keys[1];
+				Attendance.addAttendance("present", "Games", playerInt,gameId,$scope.teamId,[]); // player must be set to present!
+				//basis must be updated
+				var basis = $scope.statistics[gameId].Basis;
+				console.log(basis[extId]);
+				if(typeof basis[extId] !== 'undefined'){
+					basis[playerInt] = basis[extId];
+					delete basis[extId];
+					firebaseRef.ref().child("Statistics").child($scope.teamId).child(gameId).child("Basis").set(basis);
+				}
+				
+				//gamelog Must be  updaed and any reference to External key must be updated
+				var gameLog = $scope.statistics[gameId].GameLog;
+				for( item in gameLog ) {
+					var updated = false;
+					if(gameLog[item].player === extId){
+						gameLog[item].player = playerInt;
+						updated = true;
+					}
+					else{
+						if(gameLog[item].playerIn === extId){
+							gameLog[item].playerIn = playerInt;
+							updated = true;
+						}
+						else{
+							if(gameLog[item].playerOut === extId){
+								gameLog[item].playerOut = playerInt;
+								updated = true;
+							}
+							else{
+								if(gameLog[item].player1 === extId){
+									gameLog[item].player1 = playerInt;
+									updated = true;
+								}
+								else{
+									if(gameLog[item].player2 === extId){
+										gameLog[item].player2 = playerInt;
+										updated = true;
+									}
+								}
+							}
+						}
+					}
+					if(updated === true)
+						firebaseRef.ref().child("Statistics").child($scope.teamId).child(gameId).child("GameLog").child(item).update(gameLog[item]);
+				}
+				
+				firebaseRef.ref().child("Statistics").child($scope.teamId).child(gameId).child("externalPlayers").child(extId).update({firstName : "<removed>"});
+					
+			}
+			
+		}
     })
 
     .controller('StatisticsCtrl', function ($scope, Statistics, localStorageFactory, $filter) {
